4a685fb36634e0391920d5b49b3b94af120b575e
Eurus <exampl@gamil.com>
2025-10-08 00:02:54 +0300

    feat: add alias resolution for src directory in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      // опционально — чтобы старые импорты 'src/...' тоже работали
+      src: fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   base: "/",
   build: {
     outDir: "dist",
