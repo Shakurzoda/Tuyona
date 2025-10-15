7bef98111dc3411e22b73fca7a46e115a386cb70
Eurus <exampl@gamil.com>
2025-10-15 04:22:10 +0300

    1234

--- src/components/Pages/CategoryPage/CategoryPage.jsx
+++ src/components/Pages/CategoryPage/CategoryPage.jsx
@@ -8,8 +8,8 @@ import CardItem from "../../CardItem/CardItem";
 
 import { findCategoryBySlug } from "./categoryConfig";
 
-import OverlayLoader from "/src/components/UI/OverlayLoader/OverlayLoader";
-import { useMinDelayLoader } from "/src/components/UI/OverlayLoader/useMinDelayLoader";
+import OverlayLoader from "@/components/UI/OverlayLoader/OverlayLoader";
+import { useMinDelayLoader } from "@/components/UI/OverlayLoader/useMinDelayLoader";
 
 // утилита
 const toSlug = (s = "") =>
